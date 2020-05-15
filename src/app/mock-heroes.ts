a99272a79a9f5e268f946b425f848c5ae8dbe7fa
smuzh001 <smuzh001@ucr.edu>
2020-05-15 13:58:17 -0700

    added list styling and event binding

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import {Hero} from './hero';
+
+export const heroList: Hero[] = [
+    {id: 11, name: 'Mr Incredible'},
+    {id: 12, name: 'Bomboclatt'},
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
