9ed89461a60c3035a6f73ee3cd9ada801c0d4b79
smuzh001 <smuzh001@ucr.edu>
2020-05-17 17:49:14 -0700

    added message service and set up get method with RxJS Observable library.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
-import {heroList} from '../mock-heroes';
+import {HeroService} from '../hero.service';
+import {MessageService} from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,17 +9,25 @@ import {heroList} from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = heroList;
-  
+  heroes: Hero[];
   selectedHero: Hero;
   
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  };
   
-  constructor() { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { };
 
   ngOnInit(): void {
+    this.getHeroes();
   }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroService: Selected hero id=${hero.id}`)
+  };
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(hero => this.heroes = hero);
+  }
+  
 
 }
